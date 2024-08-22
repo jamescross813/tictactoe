699358d68a422d8498ebbacce7c4af56673930ba
jamescross813 <crossjames813@gmail.com>
2024-08-21 19:42:05 -0500

    set up human turn and ai turn functions

--- src/script.py
+++ src/script.py
@@ -26,14 +26,18 @@ def intro():
     choice = input("Are you ready for a game of Tic Tac Toe? Will you be X or O?")
     if choice == "X" or choice == "x":
         print("You have chosen X, and go first!")
+        turn()
     elif choice == "O" or choice == "o":
         print("You have chosen O, and go second!")
+        ai_turn()
     else:
         print("Try again")
         intro()
 
-
+def turn():
+    print("It's your turn")
     
-
+def ai_turn():
+    print("robo's turn")
 # board()
-# intro()
+intro()
