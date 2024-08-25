fc6ad7267a4935c30c7816fb19457119ebaee2b5
jamescross813 <crossjames813@gmail.com>
2024-08-25 18:48:52 -0500

    added self key word into cli class functions

--- src/cli.py
+++ src/cli.py
@@ -3,110 +3,109 @@ from random import randint
 import time
 
 class Cli:
-    
     spaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     turn = 0
 
-    def board():
-        print(spaces[0] + "  |  " + spaces[1] + "  |  " + spaces[2])
+    def board(self):
+        print(self.spaces[0] + "  |  " + self.spaces[1] + "  |  " + self.spaces[2])
         print("_____________")
         print ("")
-        print(spaces[3] + "  |  " + spaces[4] + "  |  "   + spaces[5])
+        print(self.spaces[3] + "  |  " + self.spaces[4] + "  |  "   + self.spaces[5])
         print("_____________")
         print ("")
-        print(spaces[6] + "  |  " + spaces[7] + "  |  " + spaces[8])
+        print(self.spaces[6] + "  |  " + self.spaces[7] + "  |  " + self.spaces[8])
 
 
-    def intro():
+    def intro(self):
         choice = input("Are you ready for a game of Tic Tac Toe? Will you be X or O?  ")
-        slow()
+        self.slow()
         if choice == "X" or choice == "x":
             print("You have chosen X, and go first!")
-            human_turn(choice)
+            self.human_turn(choice)
         elif choice == "O" or choice == "o":
             print("You have chosen O, and go second!")
-            ai_turn(shape = "X")
+            self.ai_turn(shape = "X")
         else:
             print("Try again")
-            intro()
+            self.intro()
 
     # can probably have a seperate check function later
-    def human_turn(choice):
-        slow()
+    def human_turn(self, choice):
+        self.slow()
         print("It's your turn")
-        slow()
-        if turn <=9:
+        self.slow()
+        if self.turn <=9:
             placement = input("Where do you want to place your mark?  ")
             int_placement = int(placement)
-            if spaces[(int_placement -1)] != "X" and spaces[(int_placement -1)] != "O":
-                spaces[(int_placement -1)] = choice
+            if self.spaces[(int_placement -1)] != "X" and self.spaces[(int_placement -1)] != "O":
+                self.spaces[(int_placement -1)] = choice
                 # not working, 
             else: 
                 print("That spot has already been filled, try again.")
-                human_turn(choice)
-            turn + 1
-            slow()
-            winning()
-            board()
+                self.human_turn(choice)
+            self.turn + 1
+            self.slow()
+            self.winning()
+            self.board()
             if choice == "X":
-                ai_turn("O")
+                self.ai_turn("O")
             else:
-                ai_turn("X")
+                self.ai_turn("X")
         
-    def ai_turn(shape):
-        slow()
+    def ai_turn(self, shape):
+        self.slow()
         print("robo's turn")
-        if turn <=9:
+        if self.turn <=9:
             placement = randint(1, 8)
             print(placement)
-            if spaces[(placement -1)] != "X" and spaces[(placement -1)] != "O":
-                spaces[(placement -1)] = shape
+            if self.spaces[(placement -1)] != "X" and self.spaces[(placement -1)] != "O":
+                self.spaces[(placement -1)] = shape
             else: 
-                ai_turn(shape)
-            turn + 1
-            slow()
-            board()
+                self.ai_turn(shape)
+            self.turn + 1
+            self.slow()
+            self.board()
             if shape == "X":
-                human_turn("O")
+                self.human_turn("O")
             else:
-                human_turn("X")
+                self.human_turn("X")
 
-    def winning():
-        if spaces[0] and spaces[1] and spaces[2] == "X" or spaces[0] and spaces[1] and spaces[2] == "O":
+    def winning(self):
+        if self.spaces[0] and self.spaces[1] and self.spaces[2] == "X" or self.spaces[0] and self.spaces[1] and self.spaces[2] == "O":
             print("You win!!")  
-        elif spaces[3] and spaces[4] and spaces[5] == "X" or spaces[3] and spaces[4] and spaces[5] == "O":
+        elif self.spaces[3] and self.spaces[4] and self.spaces[5] == "X" or self.spaces[3] and self.spaces[4] and self.spaces[5] == "O":
             print("You win!!")  
-        elif spaces[6] and spaces[7] and spaces[8] == "X" or spaces[6] and spaces[7] and spaces[8] == "O":
+        elif self.spaces[6] and self.spaces[7] and self.spaces[8] == "X" or self.spaces[6] and self.spaces[7] and self.spaces[8] == "O":
             print("You win!!")   
-        elif spaces[0] and spaces[3] and spaces[6] == "X" or spaces[0] and spaces[3] and spaces[6] == "O":
+        elif self.spaces[0] and self.spaces[3] and self.spaces[6] == "X" or self.spaces[0] and self.spaces[3] and self.spaces[6] == "O":
             print("You win!!")
-        elif spaces[1] and spaces[4] and spaces[7] == "X" or spaces[1] and spaces[4] and spaces[7] == "O":
+        elif self.spaces[1] and self.spaces[4] and self.spaces[7] == "X" or self.spaces[1] and self.spaces[4] and self.spaces[7] == "O":
             print("You win!!")  
-        elif spaces[2] and spaces[5] and spaces[8] == "X" or spaces[2] and spaces[5] and spaces[8] == "O":
+        elif self.spaces[2] and self.spaces[5] and self.spaces[8] == "X" or self.spaces[2] and self.spaces[5] and self.spaces[8] == "O":
             print("You win!!") 
-        elif spaces[0] and spaces[4] and spaces[8] == "X" or spaces[0] and spaces[4] and spaces[8] == "O":
+        elif self.spaces[0] and self.spaces[4] and self.spaces[8] == "X" or self.spaces[0] and self.spaces[4] and self.spaces[8] == "O":
             print("You win!!")     
-        elif spaces[6] and spaces[4] and spaces[2] == "X" or spaces[6] and spaces[4] and spaces[2] == "O":
+        elif self.spaces[6] and self.spaces[4] and self.spaces[2] == "X" or self.spaces[6] and self.spaces[4] and self.spaces[2] == "O":
             print("You win!!")  
         else:
-            ai_turn("O")
-        end()
+            self.ai_turn("O")
+        self.end()
 
-    def end():
+    def end(self):
         cont = input("Do you want to play again?  ")
         if cont == "Yes" or cont == "Y":
-            intro()
+            self.intro()
         elif cont == "No" or "N":
             print("Hope to see you again soon!")
         else:
             print("Sorry I didn't get that.....")
-            end()
+            self.end()
 
     def slow():
         time.sleep(0.5)
 
-    intro()
+    
 
     # def slow_print(input):
     #     for char in input:
