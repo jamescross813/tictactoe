9e7aa08eefa17bc17b7214a1a38408ba9890db03
jamescross813 <crossjames813@gmail.com>
2024-11-16 14:32:22 -0600

    bug hunt for issue in winning

--- src/cli.py
+++ src/cli.py
@@ -39,6 +39,7 @@ class Cli:
         print("It's your turn")
         self.slow()
         if self.turn <=9:
+            #working to this point
             placement = input("Where do you want to place your mark?  ")
             int_placement = int(placement)
             if self.spaces[(int_placement -1)] != "X" and self.spaces[(int_placement -1)] != "O":
@@ -48,7 +49,7 @@ class Cli:
                 self.human_turn()
             self.turn + 1
             self.slow()
-            self.winning("X")
+            self.winning()
             # self.board()
             # self.ai_turn()
         
@@ -70,29 +71,30 @@ class Cli:
             # self.board()
             self.human_turn()
 
-    def winning(self, shape):
-        # check rows
-        if self.board[0] == "X" and self.board[1] == "X" and self.board[2]:
-            return "you win!"
-        elif self.board[3] == "X" and self.board[4] == "X" and self.board[5]:
-            return "you win!"
-        elif self.board[6] == "X" and self.board[7] == "X" and self.board[8]:
-            return "you win!"
-        #check columns
-        elif self.board[0] == "X" and self.board[3] == "X" and self.board[5]:
-            return "you win!"
-        elif self.board[1] == "X" and self.board[4] == "X" and self.board[6]:
-            return "you win!"
-        elif self.board[2] == "X" and self.board[5] == "X" and self.board[7]:
-            return "you win!"
-        #check diagonals
-        elif self.board[0] == "X" and self.board[4] == "X" and self.board[8]:
-            return "you win!"
-        elif self.board[2] == "X" and self.board[4] == "X" and self.board[6]:
-            return "you win!"
-        else:
-            self.ai_turn()
-        # return "something"
+    def winning(self):
+        # # check rows
+        # if self.board[0] == "X" and self.board[1] == "X" and self.board[2]:
+        #     return "you win!"
+        # elif self.board[3] == "X" and self.board[4] == "X" and self.board[5]:
+        #     return "you win!"
+        # elif self.board[6] == "X" and self.board[7] == "X" and self.board[8]:
+        #     return "you win!"
+        # #check columns
+        # elif self.board[0] == "X" and self.board[3] == "X" and self.board[5]:
+        #     return "you win!"
+        # elif self.board[1] == "X" and self.board[4] == "X" and self.board[6]:
+        #     return "you win!"
+        # elif self.board[2] == "X" and self.board[5] == "X" and self.board[7]:
+        #     return "you win!"
+        # #check diagonals
+        # elif self.board[0] == "X" and self.board[4] == "X" and self.board[8]:
+        #     return "you win!"
+        # elif self.board[2] == "X" and self.board[4] == "X" and self.board[6]:
+        #     return "you win!"
+        # else:
+        #     self.ai_turn()
+        # # return "something"
+
 
     def check_turn(self):
         if self.turn > 9:
