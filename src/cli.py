3aa80985d8150c64b77fe4e69f511c0af296a58f
jamescross813 <crossjames813@gmail.com>
2024-11-16 22:24:49 -0600

    need to work out how to reset spaces variable

--- src/cli.py
+++ src/cli.py
@@ -18,8 +18,8 @@ class Cli:
         print(self.spaces[6] + "  |  " + self.spaces[7] + "  |  " + self.spaces[8])
 
 #could expand here by allowing choice of X or O
+ #works if one game played, need to blank the board
     def intro(self):
-        #works if one game played, need to blank the board and not run the choice question --> seperate function?
         choice = input("Are you ready for a game of Tic Tac Toe?   Y/N ")
         self.slow()
         if choice == "Y" or choice == "y":
@@ -32,15 +32,12 @@ class Cli:
             print("Try again")
             self.intro()
 
-    # can probably have a seperate check function later
     def human_turn(self):
-        # add in check for move, do if less the turn 9
         self.check_turn()
         self.slow()
         print("It's your turn")
         self.slow()
         if self.turn <=9:
-            #working to this point
             placement = input("Where do you want to place your mark?  ")
             int_placement = int(placement)
             if self.spaces[(int_placement -1)] != "X" and self.spaces[(int_placement -1)] != "O":
@@ -55,7 +52,6 @@ class Cli:
             return self.ai_turn()
         
     def ai_turn(self):
-        # need to include win check for ai
         self.check_turn()
         self.slow()
         if self.turn <=9:
@@ -94,8 +90,7 @@ class Cli:
             return self.win(shape)
         else:
             return "Next turn"
-        # return "something"
-
+        
     def win(self, shape):
         if shape == "X":
             print("You win!")
@@ -103,7 +98,6 @@ class Cli:
             print("You lose!")
         return self.end()
 
-    #bug in this function--> need to know how to get a cli program to close - exit()/quit()?
     def end(self):
         cont = input("Do you want to play again?  ")
         if cont == "Yes" or cont == "Y" or cont == "y":
@@ -117,12 +111,12 @@ class Cli:
 
     def reset(self):
         spaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        print(spaces)
         return self.human_turn()
 
     def check_turn(self):
         if self.turn > 9:
             print("Draw!")
-            # need actual way to end game 
             self.end()
         else:
             return "something"
@@ -131,31 +125,4 @@ class Cli:
         time.sleep(0.5)
 
 
-        # def win_ai(self):
-    #     #need to refactor these two functions into one function with variable passed in previous function
-    #     # check rows
-    #     if self.board[0] == "O" and self.board[1] == "O" and self.board[2]:
-    #         return "you win!"
-    #     elif self.board[3] == "O" and self.board[4] == "O" and self.board[5]:
-    #         return "you win!"
-    #     elif self.board[6] == "O" and self.board[7] == "O" and self.board[8]:
-    #         return "you win!"
-    #         #check columns
-    #     elif self.board[0] == "O" and self.board[3] == "O" and self.board[5]:
-    #         return "you win!"
-    #     elif self.board[1] == "O" and self.board[4] == "O" and self.board[6]:
-    #         return "you win!"
-    #     elif self.board[2] == "O" and self.board[5] == "O" and self.board[7]:
-    #         return "you win!"
-    #         #check diagonals
-    #     elif self.board[0] == "O" and self.board[4] == "O" and self.board[8]:
-    #         return "you win!"
-    #     elif self.board[2] == "O" and self.board[4] == "O" and self.board[6]:
-    #         return "you win!"
-    #     else:
-    #         self.human_turn()
-    #         # return "something"
-
-# Cli.intro()
-
-
+        
